f7ea5747013deccb558e3acbe4db10f3e9120503
Ksenia277 <shahhovakseenis.2005@mail.ru>
2024-02-21 01:44:44 +0700

    супер изменения

--- src/main.js
+++ src/main.js
@@ -1,3 +1,145 @@
+Vue.component('task', {
+    props: ['task', 'column'],
+    template: `
+      <div class="task-card">
+        <div class="task-actions" v-if="isEditingTask(task) || isFirstTask(taskIndex, column.tasks)">
+          <button @click="$emit('remove-task', task)">Удалить</button>
+        </div>
+        <input v-model="task.title" type="text" class="task-title" v-if="isEditingTask(task)">
+        <p class="task-title" v-else>{{ task.title }}</p>
+        <textarea v-model="task.description" class="task-description"></textarea>
+        <input v-model="task.deadline" type="datetime-local" class="task-deadline" @input="checkDeadline(task)">
+        <p>Дата редактирования: {{ task.lastChange | formatDate }}</p>
+        <p>Дата создания: {{ task.createdAt | formatDate }}</p>
+      </div>
+    `,
+    methods: {
+        removeTask(task) {
+            const index = this.column.tasks.indexOf(task);
+            if (index !== -1) {
+                this.column.tasks.splice(index, 1);
+            }
+        },
+        isEditingTask(task) {
+            return task.updatedAt === null;
+        },
+        isFirstTask(taskIndex, tasks) {
+            return taskIndex === 0 && tasks[taskIndex].updatedAt === null;
+        },
+        checkDeadline(task) {
+            const currentDate = new Date();
+            const deadlineDate = new Date(task.deadline);
+            console.log(task);
+            if (deadlineDate < currentDate) {
+                task.isOverdue = true;
+            }
+        }
+    }
+});
+
+
+Vue.component('task-list', {
+    props: ['tasks', 'column'],
+    template: `
+      <div class="task-list">
+        <transition-group>
+          <task v-for="(task, index) in tasks" :key="task.id" :task="task" :column="column" @remove-task="removeTask($event)"></task>
+        </transition-group>
+      </div>
+    `,
+    methods: {
+        removeTask(task) {
+            this.$emit('remove-task', task);
+        }
+    }
+});
+
+
+Vue.component('column', {
+    props: ['column'],
+    template: `
+      <div class="column">
+        <button v-if="column.title === 'Запланированные задачи'" @click="addTask(column)">Создать</button>
+        <h3>{{ column.title }}</h3>
+        <task-list :tasks="column.tasks" :column="column" @remove-task="removeTask($event)"></task-list>
+        <button v-if="column.title === 'Задачи в работе'" @click="moveToTesting(column)">Перенести в "Тестирование"</button>
+        <button v-if="column.title === 'Запланированные задачи'" @click="moveToWork(column)">Перенести в "Задачи в работе"</button>
+        <button v-if="column.title === 'Тестирование'" @click="moveToWork(column)">Обратно в "Задачи в работе"</button>
+        <input type="text" v-model="returnReason" placeholder="Reason for returning to work" class="return-reason" v-if="column.title === 'Тестирование'">
+        <column
+            v-for="column in columns"
+            :key="column.title"
+            :column="column"
+            @move-to-testing="moveTaskToTesting($event)"
+        ></column>      
+      </div>
+    `,
+    methods: {
+        addTask(column) {
+            const newTask = {
+                id: Date.now(),
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date(),
+                updatedAt: null,
+                isEditing: false,
+                initialDeadline: null,
+                isOverdue: false,
+                reasonToWork: null,
+                testingDate: null,
+            };
+            column.tasks.push(newTask);
+        },
+        moveToTesting(column) {
+            if (column.tasks.length > 0) {
+                const taskToMove = column.tasks.shift();
+                this.$emit('move-to-testing', taskToMove);
+            }
+        },
+        moveTaskToTesting(task) {
+            const testingColumn = this.columns.find(col => col.title === 'Тестирование');
+            if (testingColumn) {
+                testingColumn.tasks.push(task);
+            }
+        },
+        removeTask(task) {
+            const index = this.column.tasks.indexOf(task);
+            if (index !== -1) {
+                this.column.tasks.splice(index, 1);
+                // Обновляем состояние Vue, чтобы Vue мог отслеживать изменения
+                this.$forceUpdate();
+            }
+        },
+        moveToCompletedtasks() {
+            const testingColumn = this.columns.find(col => col.title === 'Тестирование');
+            const completedColumn = this.columns.find(col => col.title === 'Выполненные задачи');
+
+            const index = testingColumn.tasks.findIndex(task => task.title === 'Task to Move');
+
+            const task = testingColumn.tasks.splice(index, 1)[0];
+
+            this.$parent.moveToCompletedtasks(column);
+        },
+        moveToWork(column) {
+            if (column.tasks.length > 0) {
+                const taskToMove = column.tasks[0];
+                const workColumn = this.$parent.columns.find(col => col.title === 'Задачи в работе');
+                if (workColumn) {
+                    const index = column.tasks.indexOf(taskToMove);
+                    if (index !== -1) {
+                        // Удаляем задачу из исходного массива без мутации
+                        const newTasks = [...column.tasks];
+                        newTasks.splice(index, 1);
+                        column.tasks = newTasks;
+                        workColumn.tasks.push(taskToMove);
+                    }
+                }
+            }
+        }
+    }
+});
+
 const app = new Vue({
     el: '#app',
     data() {
@@ -48,84 +190,27 @@ const app = new Vue({
         }
     },
     methods: {
-        addTask(column) {
-            if (column === this.columns[0]) {
-                const newTask = {
-                    id: Date.now(),
-                    title: '',
-                    description: '',
-                    deadline: '',
-                    createdAt: new Date(),
-                    updatedAt: null,
-                    isEditing: false,
-                    initialDeadline: null,
-                    isOverdue: false,
-                    reasonToWork: null,
-                    testingDate: null,
-                };
-                column.tasks.push(newTask);
+        removeTask(task) {
+            const column = this.$parent.column;
+            const index = column.tasks.indexOf(task);
+            if (index !== -1) {
+                column.tasks.splice(index, 1);
             }
         },
-        moveTask(task, targetColumn) {
-            const sourceColumnIndex = this.columns.findIndex((column) => column.tasks.includes(task));
-            this.columns[sourceColumnIndex].tasks = this.columns[sourceColumnIndex].tasks.filter((t) => t.id !== task.id);
-            targetColumn.tasks.push(task);
-            this.updateTaskDates(task);
-        },
-        removeTask(task, column) {
-            column.tasks = column.tasks.filter((t) => t.id !== task.id);
-            this.updateTaskDates(task);
+        checkDeadline(task) {
+            const currentDate = new Date();
+            const deadlineDate = new Date(task.deadline);
+            if (deadlineDate < currentDate) {
+                task.isOverdue = true;
+            }
+            // Обновляем дату редактирования задачи
+            task.lastChange = new Date();
         },
         isEditingTask(task) {
             return task.updatedAt === null;
         },
         isFirstTask(taskIndex, tasks) {
             return taskIndex === 0 && tasks[taskIndex].updatedAt === null;
-        },
-        updateTaskDates(task) {
-            if (task.updatedAt === null) {
-                task.createdAt = new Date();
-            }
-            task.updatedAt = new Date();
-            task.lastChange = task.updatedAt;
-        },
-        moveToInProgress(column) {
-            const taskToMove = column.tasks.shift();
-            this.columns[1].tasks.push(taskToMove);
-            this.updateTaskDates(taskToMove);
-        },
-        moveToTesting(column) {
-            const taskToMove = column.tasks.shift();
-            this.columns[2].tasks.push(taskToMove);
-            this.updateTaskDates(taskToMove);
-        },
-        moveToDone(column) {
-            const taskToMove = column.tasks.shift();
-            this.columns[3].tasks.push(taskToMove);
-            this.updateTaskDates(taskToMove);
-            // checkDeadline(taskToMove);
-        },
-        moveToWork(column) {
-            if (!this.returnReason) {
-                alert('Please provide a reason for returning the task to work.');
-                return;
-            }
-
-            const taskToMove = column.tasks.shift();
-            taskToMove.returnReason = this.returnReason;
-            taskToMove.testingDate = new Date();
-            this.columns[1].tasks.push(taskToMove);
-            this.updateTaskDates(taskToMove);
-        },
-        moveToCompletedtasks() {
-            const testingColumn = this.columns.find(col => col.title === 'Тестирование');
-            const completedColumn = this.columns.find(col => col.title === 'Выполненные задачи');
-
-            const index = testingColumn.tasks.findIndex(task => task.title === 'Task to Move');
-
-            const task = testingColumn.tasks.splice(index, 1)[0];
-
-            completedColumn.tasks.push(task);
         },
         formatDate(date) {
             if (date instanceof Date) {
@@ -134,22 +219,11 @@ const app = new Vue({
                 return date;
             }
         },
-        checkDeadline(task) {
-            const currentDate = new Date();
-            const deadlineDate = new Date(task.deadline);
-            console.log(task);
-            if (deadlineDate < currentDate) {
-                task.isOverdue = true;
-            }
-        },
-    },
+    }
 });
 
 Vue.filter('formatDate', function(value) {
-    if (value instanceof Date) {
-        return value.toLocaleString();
-    } else {
-        return value;
-    }
+    if (!value) return '';
+    return new Date(value).toLocaleString();
 });
 
